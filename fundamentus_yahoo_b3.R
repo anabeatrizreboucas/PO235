d3ece2aac38e972013ff2e0ba476c1fa4bedb571
ghmunhoz <ghmunhoz@gmail.com>
2024-11-11 17:18:40 -0300

    Ajustes wd e diretórios. Filtros OK

--- fundamentus_yahoo_b3.R
+++ fundamentus_yahoo_b3.R
@@ -1,16 +1,16 @@
 # Fundamentus + Yahoo Finance
-
+# Ana Beatriz Rebouças
+# Oct 29, 2024
 rm(list = ls())
 cat("\f")
-
+  
 options(scipen = 999)
 options(digits = 6)
-
+  
 library(rvest)
 library(stringr)
 library(tidyverse)
 library(quantmod)
-
 
 # 1. Collect data from Fundamentus ####
 url <- "https://www.fundamentus.com.br/resultado.php"
@@ -21,11 +21,9 @@ html <- read_html(url)
 fund0 <- html_text(html_nodes(html, ".resultado"))
 
 # 2. Organize Fundamentus data ####
- ## Split data (split elements considering "\n" as the separator) ####
-fund0 <- str_split(fund0, "\n")
 
-## Remove blank spaces ####
-#fund0 <- str_remove_all(fund0, " ")
+## Split data (split elements considering "\n" as the separator) ####
+fund0 <- str_split(fund0, "\n")
 
 ## we have a list with only 1 element. Save as a vector ####
 fund0 <- fund0[[1]]
@@ -36,7 +34,7 @@ fund0 <- str_remove_all(fund0, "\t")
 
 ## Convert to a dataframe (21 variables) ####
 fund1 <- as.data.frame(matrix(fund0, ncol = 21,  byrow = TRUE), 
-                         stringsAsFactors = FALSE)
+                       stringsAsFactors = FALSE)
 
 ## Last row repeats the header. Remove it ####
 fund1[nrow(fund1),]
@@ -52,38 +50,37 @@ fund4 <- fund3 %>%
   mutate_all(~ str_replace(., ",", "."))
 
 ## List of variables extracted from Fundamentus ####
-    # PSL = Preço sobre lucro
-    # PSVP = Preço sobre Valor Patrimonial
-    # PSR = Preço sobre receita líquida
-    # div_yield = Dividendo sobre preço da ação
-    # PSA = Preço sobre ativos
-    # PSCG = Preço sobre capital de giro
-    # PSEBIT = Preço sobre EBIT
-    # PSACL = Preço sobre ativo circulante líquido
-    # EV_EBIT = EV/EBIT
-    # EV_EBITDA = EB/EBITDA
-    # margem_EBIT = EBIT/RL
-    # margem_liq = margem líquida
-    # liq_corrente = ativo circulante / passivo circulante
-    # ROIC = retorno sobre o capital empregado
-    # ROE = retorno sobre o PL
-    # liq_2meses = volume diário médio negociado nos ultimos 2 meses
-    # PL = patrimonio líquido
-    # div_bruta_PL = divida bruta sobre patrimônio líquido
-    # taxa_cresc_RL_5anos = taxa de crescimento da receita líquida (últimos 5 anos)
+# PSL = Preço sobre lucro
+# PSVP = Preço sobre Valor Patrimonial
+# PSR = Preço sobre receita líquida
+# div_yield = Dividendo sobre preço da ação
+# PSA = Preço sobre ativos
+# PSCG = Preço sobre capital de giro
+# PSEBIT = Preço sobre EBIT
+# PSACL = Preço sobre ativo circulante líquido
+# EV_EBIT = EV/EBIT
+# EV_EBITDA = EB/EBITDA
+# margem_EBIT = EBIT/RL
+# margem_liq = margem líquida
+# liq_corrente = ativo circulante / passivo circulante
+# ROIC = retorno sobre o capital empregado
+# ROE = retorno sobre o PL
+# liq_2meses = volume diário médio negociado nos ultimos 2 meses
+# PL = patrimonio líquido
+# div_bruta_PL = divida bruta sobre patrimônio líquido
+# taxa_cresc_RL_5anos = taxa de crescimento da receita líquida (últimos 5 anos)
 
 ## Renaming the variables ####
 names(fund4) <- c("papel", "cotacao", "PSL",
-                    "PSVP", "PSR", "div_yield",
-                    "PSA", "PSCG", "PSEBIT",
-                    "PSACL", "EV_EBIT", "EV_EBITDA", 
-                    "margem_EBIT", "margem_liq", "liq_corrente",
-                    "ROIC", "ROE", "liq_2meses",
-                    "PL", "div_bruta_PL", "taxa_cresc_RL_5anos")
-
+                  "PSVP", "PSR", "div_yield",
+                  "PSA", "PSCG", "PSEBIT",
+                  "PSACL", "EV_EBIT", "EV_EBITDA", 
+                  "margem_EBIT", "margem_liq", "liq_corrente",
+                  "ROIC", "ROE", "liq_2meses",
+                  "PL", "div_bruta_PL", "taxa_cresc_RL_5anos")
 
 ## Detect variables with values in percentage ####
-   # These variables should be divided by 100 after being converted to numeric
+# These variables should be divided by 100 after being converted to numeric
 summary_p <- fund4 %>% 
   summarize_all(~unique(str_detect(., "%"))) %>% t() 
 
@@ -92,10 +89,10 @@ summary_p <- tibble::rownames_to_column(as.data.frame(summary_p), "variable") %>
 
 summary_p
 
-  # Variables that are in percentage
+# Variables that are in percentage
 vars_p <- summary_p$variable[which(summary_p$value == TRUE)]
 
-  # Other numeric variables
+# Other numeric variables
 vars_n <- summary_p$variable[which(summary_p$value == FALSE & summary_p$variable != "papel")] 
 
 ## Converting variables to numeric ####
@@ -113,7 +110,7 @@ summary_fund <- fund5 %>%
 summary_fund
 
 fund5 %>% filter(papel %in% summary_fund$papel)
-  # Duplicates. We can simply select the unique values.
+# Duplicates. We can simply select the unique values.
 
 fund6 <- fund5 %>% unique()
 
@@ -137,107 +134,121 @@ fund <- fund6 %>%
   select(papel, descricao_papel, everything())
 
 # 3.2. Save fundamentus data ####
-  
-  # Include date information given that Fundamentus does not provide historical data
-  # and the table updates daily.
+
+# Include date information given that Fundamentus does not provide historical data
+# and the table updates daily.
 
 date <- format(Sys.Date(), "%Y%m%d")
 
-write.csv(fund, paste0(wd,"/fundamentus_data/fundamentus_", date, ".csv"))
+folder_f <- "./fundamentus_data"
+if(file.exists(folder_f)){
+  cat("folder ", folder_f, "exists\n")
+}else{
+  dir.create(folder_f, recursive = TRUE)
+}
+
+write.csv(fund, paste0(folder_f,"/fundamentus_", date, ".csv"), row.names = FALSE)
 
 
 # 4. Define stocks to be analyzed (filters) ####
-  
-  stock_sel_table <- fund %>%
+
+stock_sel_table <- fund %>%
   filter(liq_2meses >= 1000000) %>% # Liquidez > 1M
   filter(PSVP >= 0) %>%
   filter(margem_liq >= 0.1) %>%
   filter(ROE >= 0) %>%
   filter(taxa_cresc_RL_5anos >= 0)
-  
-  stock_sel <- stock_sel_table$papel
+
+stock_sel <- stock_sel_table$papel
 
 # 5. Load historical stocks data from Yahoo Finance ####
 
- ## 5.1. Load stock data from quantmod package (time series)
-    # getSymbols function from quantmod package provides stock information for a given stock 
-    # ("Symbol") and time interval. It's possible to request more than one symbol at time, but
-    # it would create individual elements for each stock. I preferred to make individual requests
-    # for each stock, store the results in a list, and then join them all in a unique table.
+## 5.1. Load stock data from quantmod package (time series)
+# getSymbols function from quantmod package provides stock information for a given stock 
+# ("Symbol") and time interval. It's possible to request more than one symbol at time, but
+# it would create individual elements for each stock. I preferred to make individual requests
+# for each stock, store the results in a list, and then join them all in a unique table.
 
-  list_aux <- list()
-  
-  for(i in 1:length(stock_sel)){
-    tryCatch({
-      stock_aux <- stock_sel[i]
-      df_aux <- getSymbols(paste0(stock_aux, ".SA"), src = "yahoo",
-                           from = as.Date('2022-01-01'), auto.assign = FALSE)
-      
-      df_aux <- as.data.frame(df_aux) %>%
-        mutate(date = row.names(.))
-      
-      row.names(df_aux) <- NULL
-      
-      list_aux[[i]] <- df_aux %>%
-        select(date, everything())
-    }, error = function(e){cat("ERROR :",conditionMessage(e), "\n")})
-  }
-  
- ## 5.2. Removing NULL entries (errors from the previous loop)  
+list_aux <- list()
 
-  list_aux <- Filter(Negate(is.null), list_aux) 
-  
- # Removing stocks listed in "stocks_sel" but not found in our data request for yahoo.
- # These stocks are detailed below (section 6.1).
+for(i in 1:length(stock_sel)){
+  tryCatch({
+    stock_aux <- stock_sel[i]
+    df_aux <- getSymbols(paste0(stock_aux, ".SA"), src = "yahoo",
+                         from = as.Date('2022-01-01'), auto.assign = FALSE)
+    
+    df_aux <- as.data.frame(df_aux) %>%
+      mutate(date = row.names(.))
+    
+    row.names(df_aux) <- NULL
+    
+    list_aux[[i]] <- df_aux %>%
+      select(date, everything())
+  }, error = function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
 
-  ## 5.3. Understanding the data structure
-    # Each element of the list (data of each stock) will have specific column names,
-    # based on their stock name. For example:  
-  
-  head(list_aux[[1]]) 
-  head(list_aux[[2]]) 
-  
-  # The prefix of the column name is the stock name and the suffix is the variable name 
-  # ("Open", "High", "Low", "Close", "Volume". and "Adjusted"). 
-  
-  
-  ## 5.4. Joining all elements (all stocks) in one table 
-  df <- list_aux %>%
-    reduce(full_join, by = "date")
+## 5.2. Removing NULL entries (errors from the previous loop)  
 
-  # This data is not tidy yet. Stocks data is distributed across columns and not rows. 
-  # Below an extract of the dataset is presented (first 6 rows and first 8 columns).  
-  head(df)[,1:8]
-  
-  ## 6. Making the data tidy ####
-  df0 <- df %>%
-    pivot_longer(cols = -date,
-                 names_to = "variable",
-                 values_to = "value",
-                 values_drop_na = TRUE)
-  
-  head(df0)
+list_aux <- Filter(Negate(is.null), list_aux) 
 
-   # Not tidy yet. All variables are stored in one column named "variable" and their value
-   # is stored in a column named "value". This column also contains the stock name. I 
-   # separate this information into 2 variables: 
-      ### "stock": the stock name, and
-      ### "var": variable name  
-  
-  df0 <- df0 %>%
-    mutate(stock = str_extract(variable,".*(?=\\.SA)"),
-           var = str_extract(variable, "(?<=SA\\.).*"))
-  
-  head(df0)
+# Removing stocks listed in "stocks_sel" but not found in our data request for yahoo.
+# These stocks are detailed below (section 6.1).
 
- df1 <- df0 %>% select(-variable) %>%
-    pivot_wider(names_from = var,
-                values_from = value) %>%
-    select(stock, date, everything()) %>%
-    arrange(stock, date)
-  
-  head(df1)  
+## 5.3. Understanding the data structure
+# Each element of the list (data of each stock) will have specific column names,
+# based on their stock name. For example:  
 
-  # df1 is now tidy.
- 
-   write.csv(df1, file = paste0(wd,"/yahoo_data/yahoo_b3_", date, ".csv"), row.names = FALSE)  
+head(list_aux[[1]]) 
+head(list_aux[[2]]) 
+
+# The prefix of the column name is the stock name and the suffix is the variable name 
+# ("Open", "High", "Low", "Close", "Volume". and "Adjusted"). 
+
+
+## 5.4. Joining all elements (all stocks) in one table 
+df <- list_aux %>%
+  reduce(full_join, by = "date")
+
+# This data is not tidy yet. Stocks data is distributed across columns and not rows. 
+# Below an extract of the dataset is presented (first 6 rows and first 8 columns).  
+head(df)[,1:8]
+
+## 6. Making the data tidy ####
+df0 <- df %>%
+  pivot_longer(cols = -date,
+               names_to = "variable",
+               values_to = "value",
+               values_drop_na = TRUE)
+
+head(df0)
+
+# Not tidy yet. All variables are stored in one column named "variable" and their value
+# is stored in a column named "value". This column also contains the stock name. I 
+# separate this information into 2 variables: 
+### "stock": the stock name, and
+### "var": variable name  
+
+df0 <- df0 %>%
+  mutate(stock = str_extract(variable,".*(?=\\.SA)"),
+         var = str_extract(variable, "(?<=SA\\.).*"))
+
+head(df0)
+
+df1 <- df0 %>% select(-variable) %>%
+  pivot_wider(names_from = var,
+              values_from = value) %>%
+  select(stock, date, everything()) %>%
+  arrange(stock, date)
+
+head(df1)  
+
+# df1 is now tidy.
+
+folder_y <- "./yahoo_data"
+if(file.exists(folder_y)){
+  cat("folder ", folder_y, "exists\n")
+}else{
+  dir.create(folder_y, recursive = TRUE)
+}
+
+write.csv(fund, paste0(folder_y,"/yahoo_data", date, ".csv"), row.names = FALSE)
